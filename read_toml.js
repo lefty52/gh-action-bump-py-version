22994c7da7e5e6bb6f616e3eb74322741fe9b763
winton <winton_booysen@outlook.com>
2025-08-28 00:59:45 +0200

    Update toml works

--- read_toml.js
+++ read_toml.js
@@ -33,6 +33,7 @@
     // }
 
 import { parse, stringify } from 'smol-toml';
+import fs from 'fs';
 
 // Example TOML string
 const tomlString = `
@@ -49,6 +50,26 @@ dob = 1990-01-01T12:00:00Z
 `;
 
 // Parse the TOML string into a JavaScript object
-const parsedObject = parse(tomlString);
-console.log('Parsed Object:', parsedObject);
-console.log('Parsed Object:', parsedObject.database);
+// const parsedObject = parse(tomlString);
+// console.log('Parsed Object:', parsedObject);
+// console.log('Parsed Object:', parsedObject.database);
+let pathToPyproject = '/home/winton/VSCode/gh-action-bump-py-version/pyproject.toml';
+
+if (!fs.existsSync(pathToPyproject)) throw new Error(pyprojectTOMLFileName + " could not be found in your project's root.");
+    console.log('pathToPyproject:', pathToPyproject);
+    try {
+      const fileContent = fs.readFileSync(pathToPyproject, 'utf8');
+      const parsedToml = parse(fileContent);
+      console.log('parsedToml:', parsedToml);
+      console.log('fileContent project:', parsedToml.project);
+      console.log('fileContent.project.version:', parsedToml.project.version);
+      
+
+//     // return parsedToml;
+  } catch (error) {
+    console.error(`Error reading or parsing pyproject.toml: ${error.message}`);
+    // return null;
+  }
+    // Parse the TOML string into a JavaScript object
+    // const parsedObject = parse(pathToPyproject);
+    // console.log('Parsed Object:', parsedObject);
